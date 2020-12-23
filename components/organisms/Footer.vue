4b216871a72135badeb6b036817f971e27b3faf2
Pedro Cruz <pedropcruz@outlook.pt>
2020-12-23 16:49:05 +0000

    fix footer on mobile

--- components/organisms/Footer.vue
+++ components/organisms/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <footer class="footer">
     <div class="container has-text-centered">
-      <div class="content is-flex is-justify-content-center">
+      <div class="content is-flex is-justify-content-center is-flex-wrap-wrap">
         <a
           v-for="(social, index) in data"
           :key="index"
           :href="social.to"
           target="_blank"
           rel="noopener noreferrer"
-          class="link"
+          class="m-3"
         >
-          <figure class="image is-24x24 mx-3">
+          <figure class="image is-24x24 mx-0">
             <img
               :class="getTheme + `-img`"
               :src="social.url[getTheme]"
